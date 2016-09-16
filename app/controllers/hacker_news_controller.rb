ca1b31fc43c3cdaeff5be0d0a1e6c3dc6f35a069
okhwaja <osman.khwaja@gmail.com>
2016-09-16 07:53:45 -0700

    starting

--- app/controllers/hacker_news_controller.rb
+++ app/controllers/hacker_news_controller.rb
@@ -1,2 +1,16 @@
 class HackerNewsController < ApplicationController
+  helper HackerNewsHelper
+  def index
+    data = [
+      {
+        one: 'two',
+        three: 'four'
+      },
+      {
+        one: 'two',
+        three: 'four'
+      }
+    ]
+    render xml: data
+  end
 end
